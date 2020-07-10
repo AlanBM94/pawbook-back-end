8029e617b436f7990d1bf44e260f831f2466519c
alanbm94 <sevilbm94@hotmail.com>
2020-07-10 14:21:07 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
